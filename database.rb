d2eb475e0be28bb86c571d4c681b1d08d0b30586
dinesh <you@example.com>
2016-01-18 15:11:06 +0545

    test

--- database.rb
+++ database.rb
@@ -3,9 +3,5 @@ require 'mysql2'
 begin
   con = Mysql.new 'localhost', 'root', 'dinesh 770'
   con.query("CREATE DATABASE IF NOT EXISTS usershare") 
-rescue Mysql::Error => e
-  puts e.errno
-  puts e.error
-ensure
   con.close if con
 end
